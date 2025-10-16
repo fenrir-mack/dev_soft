edd78490dca4f1c62c5d8c174c161be06bda4309
fenrir-mack <10436455@mackenzista.com.br>
2025-10-15 23:23:19 -0300

    implementadando nickname e manter usuario no cadastro quando erro

--- backend/users/views.py
+++ backend/users/views.py
@@ -8,11 +8,13 @@ User = get_user_model()
 
 
 def index_view(request):
+    active_tab = 'login'  # default
+
     if request.method == 'POST':
         type_access = request.POST.get('action')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        print(email, password)
+
         if type_access == "login":
             user = authenticate(request, username=email, password=password)
             if user is not None:
@@ -20,18 +22,29 @@ def index_view(request):
                 return redirect('trilhas:dashboard')
             else:
                 messages.error(request, 'Email ou senha incorretos.')
-                return redirect('login')
+                active_tab = 'login'
         elif type_access == 'signup':
             name = request.POST.get('name')
+            nickname = request.POST.get('nickname')
             confirm_password = request.POST.get('confirm_password')
+            active_tab = 'signup'  # make signup tab active
 
             if password != confirm_password:
                 messages.error(request, 'As senhas não coincidem.')
             elif User.objects.filter(username=email).exists():
                 messages.error(request, 'Email já registrado.')
             else:
-                user = User.objects.create_user(username=email, email=email, password=password, full_name=name)
+                user = User.objects.create_user(username=email, email=email, password=password,full_name=name, nickname=nickname)
                 login(request, user)
                 return redirect('trilhas:dashboard')
 
-    return render(request, 'users/index.html')
+        # If there’s an error, render the page with the correct tab and pre-fill email/name
+        context = {
+            'active_tab': active_tab,
+            'prefill_email': email,
+            'prefill_name': request.POST.get('name', ''),
+            'prefill_nickname': request.POST.get('nickname', ''),
+        }
+        return render(request, 'users/index.html', context)
+
+    return render(request, 'users/index.html', {'active_tab': active_tab})
